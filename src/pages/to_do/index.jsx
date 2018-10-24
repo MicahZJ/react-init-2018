c80b9f7d3245679e304768c37766f13dfa64b416
MicahZj <miko117123@gmail.com>
2018-10-24 18:39:34 +0800

    增加react-slick配置

--- src/pages/to_do/index.jsx
+++ src/pages/to_do/index.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Style from './style.styl'
 import { observer, inject } from 'mobx-react';
 import {withRouter} from "react-router-dom"
+import Slider from 'react-slick'
 
 @withRouter
 @inject('todoStore')
@@ -20,8 +21,40 @@ export default class AddTodo extends Component {
   }
 
 	render() {
+    const settings = {
+      className: "center",
+      centerMode: true,
+      infinite: true,
+      centerPadding: "40px",
+      slidesToShow: 1,
+      speed: 500,
+      dots: true,
+      arrows:false
+    };
 		return(
-			<div>{this.store.a}</div>
+			<div>{this.store.a}
+        <h2>Center Mode</h2>
+        <Slider {...settings}>
+          <div>
+            <h3 style={{height: '300px'}}>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          {/*<div>*/}
+          {/*<h3>4</h3>*/}
+          {/*</div>*/}
+          {/*<div>*/}
+          {/*<h3>5</h3>*/}
+          {/*</div>*/}
+          {/*<div>*/}
+          {/*<h3>6</h3>*/}
+          {/*</div>*/}
+        </Slider>
+			</div>
 		)
 	}
 }
